1546b2e58e9eb5a7f406e2aae2fd81c8ebb981ce
Hieu Tran <103094887+TNH510@users.noreply.github.com>
2023-07-27 16:49:58 +0700

    This is a combination of 6 commits.
    Fix: Wrong Pin
    
    Create: FSM_GPIO Diagram
    
    Complete: FSM GPIO Diagram
    
    Update: 1/2 case
    
    Release: Version 1.0 GPIO FSM
    
    Update: FSM GPIO Diagram

--- FSM/FSM/Core/Inc/main.h
+++ FSM/FSM/Core/Inc/main.h
@@ -58,12 +58,12 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define R_Pin GPIO_PIN_1
-#define R_GPIO_Port GPIOA
+#define G_Pin GPIO_PIN_1
+#define G_GPIO_Port GPIOA
 #define B_Pin GPIO_PIN_2
 #define B_GPIO_Port GPIOA
-#define RA3_Pin GPIO_PIN_3
-#define RA3_GPIO_Port GPIOA
+#define R_Pin GPIO_PIN_3
+#define R_GPIO_Port GPIOA
 #define BUTTON_Pin GPIO_PIN_7
 #define BUTTON_GPIO_Port GPIOA
 #define BUTTON_EXTI_IRQn EXTI9_5_IRQn
